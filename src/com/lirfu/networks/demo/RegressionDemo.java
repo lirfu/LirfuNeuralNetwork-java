02326dba79907237b4bc2449e48dd5b754be1e64
lirfu <jurajfulir@gmail.com>
2017-12-15 21:46:41 +0100

    Modify the demo

--- src/com/lirfu/networks/demo/RegressionDemo.java
+++ src/com/lirfu/networks/demo/RegressionDemo.java
@@ -1,8 +1,11 @@
 package com.lirfu.networks.demo;
 
 import com.lirfu.graphicslib.functions.Linear;
-import com.lirfu.lirfugraph.*;
+import com.lirfu.lirfugraph.Row;
+import com.lirfu.lirfugraph.VerticalContainer;
 import com.lirfu.lirfugraph.Window;
+import com.lirfu.lirfugraph.DualLinearGraph;
+import com.lirfu.lirfugraph.LinearGraph;
 import com.lirfu.networks.DataSeparator;
 import com.lirfu.networks.Network;
 import com.lirfu.graphicslib.IncompatibleOperandException;
@@ -13,8 +16,6 @@ import com.lirfu.graphicslib.vector.Vector;
 import com.lirfu.networks.SeparatedData;
 import com.lirfu.networks.layers.FullyConnectedLayer;
 import com.lirfu.networks.layers.InputLayer;
-
-import java.awt.*;
 
 /**
  * Created by lirfu on 08.08.17..
@@ -31,12 +32,11 @@ public class RegressionDemo {
         for (int i = 0; i < numberOfPoints; i++) {
             input = i * 2. * Math.PI / numberOfPoints;
             inputs[i] = new Matrix(new Vector(input));
-            outputs[i] = new Matrix(new Vector(4 * Math.sin(2 * input) + 2));
+            outputs[i] = new Matrix(new Vector(4 * Math.sin(2 * input) + 5));
         }
 
         /* Build the network. */
         Network net = new Network(
-                1e-4,
                 new InputLayer(1),
                 new FullyConnectedLayer(1, 6, new Sigmoid()),
                 new FullyConnectedLayer(6, 1, new Linear())
@@ -49,10 +49,10 @@ public class RegressionDemo {
         int inputIndex = 3;
         DualLinearGraph resultsGraph = new DualLinearGraph("Results for input #" + inputIndex);
 
-        SeparatedData data = DataSeparator.separateData(inputs, outputs, 0.8);
+        SeparatedData[] data = DataSeparator.toStocastic(DataSeparator.separateData(inputs, outputs, 0.8));
         int iteration = 0;
         double error, result;
-        while ((error = net.backpropagate(data)) > 1e-3) {
+        while ((error = net.backpropagate(1e-3, data)) > 1e-1) {
             if (iteration++ % 1000 == 0) {
                 result = net.getOutput(inputs[inputIndex]).get(0, 0);
                 errorsGraph.add(error);
@@ -65,7 +65,7 @@ public class RegressionDemo {
 
         System.out.println("Weights:\n" + net.toString());
 
-        DualLinearGraph finalResults = new DualLinearGraph("Final results");
+        DualLinearGraph finalResults = new com.lirfu.lirfugraph.DualLinearGraph("Final results");
         for (int index = 0; index < inputs.length; index++)
             finalResults.add(net.getOutput(inputs[index]).get(0, 0), outputs[index].get(0, 0));
 
